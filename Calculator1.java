637e5c4f5efb5f3990b86c1fbef69da0080078bd
abhi5926 <abhisheksen5926@gmail.com>
2025-01-07 18:33:03 +0530

    Welcomes

---
+++ Calculator1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Calculator1{
+  public static void main(String args[]){
+  System.out.println("Welcome to calculator");
+  Scanner sc=new Scanner(System.in);
+  int a;
+  System.out.println("Enter the first number");
+  a=sc.nextInt();
+  int b;
+  System.out.println("Enter the second number");
+  b=sc.nextInt();
+  int ch;
+  System.out.println("Enter your choice");
+  ch=sc.nextInt();
+  }
+}
