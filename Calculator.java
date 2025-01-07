9caf166e63fa8ac2ae10d9f8d8825c7336aa6268
abhi5926 <abhisheksen5926@gmail.com>
2025-01-07 14:52:06 +0530

    Addition

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,21 @@
 class Calculator{
     public static void main(String[] args) {
-        System.out.println("Welcome to Calculator");
+        System.out.println("Welcome to Calculator");   
+ 	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Two Number");
+	int fno=sc.nextInt();
+	int sno=sc.nextInt();
+	System.out.println("Enter your choice \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division");
+        int choice=sc.nextInt();
+	int result=0;
+      	switch(choice){
+	case 1:
+	result=fno+sno;
+	break;
+	default:
+	System.out.println("Enter a valid choice");
+	}
+	System.out.println("Your result is "+result);
     }
 
 }
