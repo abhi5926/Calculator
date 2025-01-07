785ec442ef7914f41f0de4f8c7645c0b63ac956a
abhi5926 <abhisheksen5926@gmail.com>
2025-01-07 12:07:24 +0530

    Welcome

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Add{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int a=0;
+System.out.println("Enter first number");
+a=sc.nextInt();
+int b=0;
+System.out.println("Enter second number");
+b=sc.nextInt();
+int c=a+b;
+System.out.println("add="+c);
+}
+}
